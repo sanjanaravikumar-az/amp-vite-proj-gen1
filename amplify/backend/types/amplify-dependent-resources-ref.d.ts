d25139048c98d0cb19e37931e85a5c42c2bbfd1f
sanjanaravikumar-az <sanjrkmr@amazon.com>
2025-10-31 10:58:33 -0400

    Initial React + TypeScript + Vite template

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "viteproject": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "custom": {
+    "customResourceb21d659e": {
+      "snsTopicArn": "string"
+    }
+  }
+}
